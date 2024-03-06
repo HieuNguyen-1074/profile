b19658cbbe440eb501cae7df86209ed8154f39ca
Hieu Nguyen <hieunv2@bssd.vn>
2024-03-06 16:05:47 +0700

    Fix bug: pdf too big when user using lc screen

--- product/my-app/app/(api)/constant.ts
+++
@@ -1,9 +0,0 @@
-
-export const URL_API_LOGIN = process.env.URL_API_LOGIN
-export const  URL_FETCH_DATA = process.env.URL_FETCH_DATA
-
-export const statusAPIsuccess = [201 ,200 , 204,203 , 204]
-
-
-export const  URL_LOGIN = "auth/login" ;
-export const URL_GET_USER = "users/" ;
